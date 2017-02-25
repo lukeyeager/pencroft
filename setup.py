e5395dd3ac1f64c6090fa59eb6c826f3f6e035a8
Luke Yeager <lukeyeager@users.noreply.github.com>
2017-02-24 16:07:38 -0800

    Mark v0.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(LOCAL_DIR, 'requirements-test.txt'), 'r') as infile:
 
 setuptools.setup(
     name='pencroft',
-    version='0.4',
+    version='0.5',
     license='BSD',
     packages=setuptools.find_packages(),
     extras_require={'test': test_requirements},
